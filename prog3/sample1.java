9332a574e21d8122a9003731eaf7aa495af6fab4
kingslydarcy <111500924+kingslydarcy@users.noreply.github.com>
2022-09-20 23:57:10 +0530

    Create sample1.java

---
+++ prog3/sample1.java
@@ -0,0 +1,13 @@
+package sample_1;
+
+public class sample1 {
+    protected void display()
+    {
+        System.out.println("protected property");
+    }
+    void display1()
+    {
+        System.out.println("default property");
+    }
+   
+}
