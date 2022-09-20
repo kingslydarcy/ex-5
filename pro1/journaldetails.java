52d50b2762c0c3409f3c7266ac5a73bd4fab3d3e
kingslydarcy <111500924+kingslydarcy@users.noreply.github.com>
2022-09-20 23:48:21 +0530

    Create journaldetails.java

---
+++ pro1/journaldetails.java
@@ -0,0 +1,32 @@
+package journal;
+
+public class Journaldetails {
+    int journalid;
+    String journalname;
+    int journaledition;
+    public Journaldetails( int journalid,String journalname,int journaledition){
+        this.journalid=journalid;
+        this.journalname=journalname;
+        this.journaledition=journaledition;
+    }
+    public void show1(){
+        System.out.println("journal id"+journalid);
+        System.out.println("journal name"+journalname);
+        System.out.println("journal edition"+journaledition);
+
+    }
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
