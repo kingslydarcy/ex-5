e65d66f6e597c5a401618d8fce756ef35b650c4f
kingslydarcy <111500924+kingslydarcy@users.noreply.github.com>
2022-09-20 23:50:17 +0530

    Create mainpack.java

---
+++ prog2/mainpack.java
@@ -0,0 +1,17 @@
+package main;
+import main.sub1.pack1;
+import main.sub2.pack2;
+import main.sub3.pack3;
+public class mainpack
+{
+    public static void main(String[] args) 
+    {
+        pack1 p1=new pack1();
+        pack2 p2=new pack2();
+        pack3 p3=new pack3();
+        p1.show1();
+        p2.show2();
+        p3.show3();
+    }
+   
+}
